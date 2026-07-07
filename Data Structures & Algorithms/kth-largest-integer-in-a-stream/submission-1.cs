4a7d58a14514baa49d7d20c5775daf4a6959742d
Shweta Patel <84307434+shwetaptl@users.noreply.github.com>
2026-07-07 11:55:28 -0700

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cs
@@ -0,0 +1,31 @@
+public class KthLargest {
+
+    int k;
+    int[] nums;
+    PriorityQueue<int,int> queue = new PriorityQueue<int,int>();
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.nums = nums;
+
+        for(int i = 0 ; i < nums.Length; i++)
+        {
+            queue.Enqueue(nums[i],nums[i]);
+            if(queue.Count > k)
+            {   
+                queue.Dequeue();
+            }
+        }
+    }//nlogk
+    
+    public int Add(int val) {
+        queue.Enqueue(val,val);
+        if(queue.Count > k)
+        {   
+            queue.Dequeue();
+        }
+        return queue.Peek();
+    }//logk
+
+    //space = O(k)
+}
