aa6d063491b58db967d4925a16a5fe3149124b39
Shweta Patel <84307434+shwetaptl@users.noreply.github.com>
2026-07-07 15:54:23 -0700

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LeastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        int maxFreq = 0 ; 
+        int maxCount = 0 ; 
+        foreach(char c in tasks)
+        {
+            freq[c - 'A']++;
+            maxFreq = Math.Max(maxFreq, freq[c - 'A']);
+        }
+
+        for(int i = 0 ; i < 26; i++)
+        {
+            if(freq[i] == maxFreq)
+            {
+                maxCount++;
+            }
+        }
+        int total = ((maxFreq - 1) * (n+1)) + maxCount;
+        return Math.Max(tasks.Length, total); // space = O(1) , time O(m)
+        //tasks = ['A','A','A','B','B','B','C','C','D','D']
+        //n = 2
+        //cycle require = 10
+    }
+}
